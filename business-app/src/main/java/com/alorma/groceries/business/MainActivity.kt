191cd060e4a8a51baf33b287ead13bfd61aaf369
Bernat Borrás <bernat.borras@adevinta.com>
2022-03-17 18:14:48 +0100

    Add some scroll feedback

--- business-app/src/main/java/com/alorma/groceries/business/MainActivity.kt
+++ business-app/src/main/java/com/alorma/groceries/business/MainActivity.kt
@@ -9,12 +9,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import com.alorma.groceries.business.ui.theme.BusinessTheme
 import com.alorma.groceries.uikit.Section
@@ -27,12 +32,26 @@ class MainActivity : ComponentActivity() {
 
     setContent {
       val items = remember { mutableListOf("Schedule", "Stock", "Orders", "Clients", "Settings") }
-      BusinessTheme {
-        Scaffold {
-          Column(
 
+      BusinessTheme {
+        val scrollBehavior = remember { TopAppBarDefaults.pinnedScrollBehavior() }
+
+        Scaffold(
+          modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+          topBar = {
+            CenterAlignedTopAppBar(
+              title = { Text(text = "Fruteria Pili") },
+              scrollBehavior = scrollBehavior,
+            )
+          }
+        ) { innerPadding ->
+          LazyColumn(
+            contentPadding = innerPadding,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
           ) {
-            ManageOptions(items)
+            item(key = { "manage options" }) {
+              ManageOptions(items)
+            }
           }
         }
       }
